5486e72cc66f739558509c015813878fd404bb7d
shiridisai07 <chinnamshiridisai@gmail.com>
2026-03-31 09:44:48 +0530

    Added frontend and backend project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserForm from './UserForm'
+import UserList from './UserList'
+
+function App() {
+  return (
+    <div style={{textAlign:"center"}}>
+
+      <h1>Skill 13 Full Stack Deployment</h1>
+
+      <UserForm/>
+      <UserList/>
+
+    </div>
+  )
+}
+
+export default App
